b788636d654b5d90067c20d8aa5880bfb0a574af
car <carmelogustavo@gmail.com>
2019-03-01 01:09:39 +0100

    feat: declare app, first middleware and initial route

---
+++ src/app/app.ts
@@ -0,0 +1,27 @@
+import * as Koa from 'koa';
+import * as HttpStatus from 'http-status-codes';
+
+const app: Koa = new Koa();
+
+// Initial generic error handling middleware.
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.status =
+      error.statusCode || error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    error.status = ctx.status;
+    ctx.body = { error };
+    ctx.app.emit('error', error, ctx);
+  }
+});
+
+// Initial route
+app.use(async (ctx: Koa.Context) => {
+  ctx.body = 'Start your engines!';
+});
+
+// Application error logging.
+app.on('error', console.error);
+
+export default app;
