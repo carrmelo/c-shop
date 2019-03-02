2f44a4b359d15819a81ff8b270c8177416042b49
car <carmelogustavo@gmail.com>
2019-03-02 13:24:18 +0100

    refactor: comments out and TODO in

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -2,7 +2,13 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import { getRepository, Repository } from 'typeorm';
 import customerEntity from './customer.entity';
-import * as HttpStatus from 'http-status-codes';
+import {
+  OK,
+  NOT_FOUND,
+  CREATED,
+  NO_CONTENT,
+  ACCEPTED,
+} from 'http-status-codes';
 
 const routerOpts: Router.IRouterOptions = {
   prefix: '/customers',
@@ -11,47 +17,41 @@ const routerOpts: Router.IRouterOptions = {
 const router: Router = new Router(routerOpts);
 
 router.get('/', async (ctx: Koa.Context) => {
-  // Get the customer repository
+  // TODO Authentication
   const customerRepo: Repository<customerEntity> = getRepository(
     customerEntity,
   );
 
-  // Find the requested customer
   const customers = await customerRepo.find();
 
-  // Respond with the customer data
-  ctx.status = HttpStatus.OK;
+  ctx.status = OK;
   ctx.body = { data: { customers } };
 });
 
 router.get('/:customer_id', async (ctx: Koa.Context) => {
-  // Get the customer repository
+  // TODO Authentication
   const customerRepo: Repository<customerEntity> = getRepository(
     customerEntity,
   );
 
-  // Find the requested customer
   const customer = await customerRepo.findOne(ctx.params.customer_id);
 
-  // If there's no customer throw to Error Handler middleware
   if (!customer) {
-    ctx.throw(HttpStatus.NOT_FOUND);
+    ctx.throw(NOT_FOUND);
   }
 
-  // Respond with customer
-  ctx.status = HttpStatus.OK;
+  ctx.status = OK;
   ctx.body = { data: { customer } };
 });
 
 router.post('/', async (ctx: Koa.Context) => {
-  // Get the customer repository
+  // TODO Authentication
   const customerRepo: Repository<customerEntity> = getRepository(
     customerEntity,
   );
 
   const { name, surname, pictureUrl } = ctx.request.body;
-
-  // Create the customer and save it to the DB
+  // TODO createdBy
   const customer: customerEntity = customerRepo.create({
     name,
     surname,
@@ -59,52 +59,43 @@ router.post('/', async (ctx: Koa.Context) => {
   });
   const newCustomer = await customerRepo.save(customer);
 
-  // Respond with the created customer
-  ctx.status = HttpStatus.CREATED;
+  ctx.status = CREATED;
   ctx.body = { data: newCustomer };
 });
 
 router.delete('/:customer_id', async (ctx: Koa.Context) => {
-  // Get the customer repo
+  // TODO Authentication
   const customerRepo: Repository<customerEntity> = getRepository(
     customerEntity,
   );
 
-  // Find the requested customer
   const customer = await customerRepo.findOne(ctx.params.customer_id);
 
-  // If there's no customer throw to Error Handler middleware
   if (!customer) {
-    ctx.throw(HttpStatus.NOT_FOUND);
+    ctx.throw(NOT_FOUND);
   }
 
-  // Delete our customer
   await customerRepo.delete(customer);
 
-  // Respond with NO CONTENT
-  ctx.status = HttpStatus.NO_CONTENT;
+  ctx.status = NO_CONTENT;
 });
 
 router.patch('/:customer_id', async (ctx: Koa.Context) => {
-  // Get the customer repo
+  // TODO Authentication
   const customerRepo: Repository<customerEntity> = getRepository(
     customerEntity,
   );
 
-  // Find the requested customer
   const customer = await customerRepo.findOne(ctx.params.customer_id);
 
-  // If there's no customer throw to Error Handler middleware
   if (!customer) {
-    ctx.throw(HttpStatus.NOT_FOUND);
+    ctx.throw(NOT_FOUND);
   }
-
-  // Merge the existing customer with the new data
+  // TODO lastModifiedBy
   const updatedCustomer = await customerRepo.merge(customer, ctx.rquest.body);
   customerRepo.save(updatedCustomer);
 
-  // Respond with ACCEPTED status and updated data
-  ctx.status = HttpStatus.ACCEPTED;
+  ctx.status = ACCEPTED;
   ctx.body = { data: { customer: updatedCustomer } };
 });
 
